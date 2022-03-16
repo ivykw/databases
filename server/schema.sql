2ca96b0163befd3724f8a7768bd0fe650e5181d0
ivykw <ivyw219@gmail.com>
2022-03-16 18:43:50 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Create other tables and define schemas for them here! */
+
+CREATE TABLE usernames (
+ id INT NOT NULL AUTO_INCREMENT,
+ username VARCHAR(20),
+ friend CHAR(1),
+ PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE roomnames (
+  id INT NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(15),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(50),
+  username_id INT,
+  roomname_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (username_id)
+    REFERENCES usernames(id),
+  FOREIGN KEY (roomname_id)
+    REFERENCES roomnames(id)
+);
+
 
 
 
